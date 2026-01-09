776d58d93a23d00d5c764cb46e350f7ec223cf62
Minsung <qkralstjd02@gmail.com>
2026-01-09 14:32:17 +0900

    refactor: 네트워크 관련 파일들을 :core:network 모듈로 이동 및 정리

--- core/network/src/main/java/com/vibe/hub/core/network/CoordConverterService.kt
+++ core/network/src/main/java/com/vibe/hub/core/network/CoordConverterService.kt
@@ -1,17 +1,12 @@
-package com.vibe.hub.data.api
+package com.vibe.hub.core.network
 
 import kr.hyosang.coordinate.CoordPoint
 import kr.hyosang.coordinate.TransCoord
 import javax.inject.Inject
 import javax.inject.Singleton
 
-/**
- * CoordPoint.jar 라이브러리를 사용하여 GPS(WGS84) 좌표를 TM 좌표로 변환합니다.
- * 에어코리아 측정소 찾기 등에서 활용됩니다.
- */
 @Singleton
 class CoordConverterService @Inject constructor() {
-
     fun convertWgs84ToTm(lat: Double, lon: Double): Pair<Double, Double> {
         val pt = CoordPoint(lon, lat)
         val tmPt = TransCoord.getTransCoord(pt, TransCoord.COORD_TYPE_WGS84, TransCoord.COORD_TYPE_TM)
